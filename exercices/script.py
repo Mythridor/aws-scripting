79279bdf1a5aad15ee45adf6c0b5360a8bc86d12
Mythridor <enoubia@gmail.com>
2017-10-12 14:16:21 +0200

    Uploading Auto-scaling UserData Exercise by adding substancially Read Replica (FF)

--- exercices/script.py
+++ exercices/script.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 import cgitb
+import collections
+import json
 
 import pymysql
 import requests
@@ -7,7 +9,7 @@ import requests
 pymysql.install_as_MySQLdb()
 
 cgitb.enable()
-print("Content-Type: text/plain")
+print("Content-Type: text/html; charset=utf-8")
 print()
 instance = "http://169.254.169.254/latest/meta-data/instance-id"
 
@@ -16,29 +18,40 @@ def instanceId(url):
     instance_id = requests.get(url)
     return instance_id.content
 
-
-print(instanceId(instance).decode("utf-8"))
 try:
-    db = pymysql.connect(host="benmysqlinstance.c7p9s8bjlzfz.eu-west-1.rds.amazonaws.com",
-                         # your host, usually localhost
-                         user="Mythridor",  # your username
-                         passwd="3141592653589Rr",  # your password
-                         db="test")  # name of the data base
+    writedb = pymysql.connect(host="benmysqlinstance.c7p9s8bjlzfz.eu-west-1.rds.amazonaws.com",
+                              user="Mythridor",
+                              passwd="3141592653589Rr",
+                              db="test")
 
-    cur = db.cursor()
-    cur.execute("SELECT * FROM instances")
+    readdb = pymysql.connect(host="benmysqlinstancereplica.c7p9s8bjlzfz.eu-west-1.rds.amazonaws.com",
+                             user="Mythridor",
+                             passwd="3141592653589Rr",
+                             db="test")
+    cur = readdb.cursor()
+    writecur = writedb.cursor()
     ctr = 0
     inserted = False
+    cur.execute("SELECT * FROM instances")
     for row in cur.fetchall():
-        print(row)
         if row[1] == instanceId(instance).decode("utf-8"):
-            response = cur.execute("UPDATE instances SET ctr = ctr + 1 WHERE id = %s", row[0])
+            response = writecur.execute("UPDATE instances SET ctr = ctr + 1 WHERE id = %s", row[0])
             inserted = True
     if inserted == False:
-        response = cur.execute("INSERT INTO instances (instance_id, ctr) VALUE (%s, %s)",
-                               (instanceId(instance).decode("utf-8"), ctr))
-        print(response)
-    db.commit()
-    db.close()
-except:
-    print("error")
+        response = writecur.execute("INSERT INTO instances (instance_id, ctr) VALUE (%s, %s)",
+                                    (instanceId(instance).decode("utf-8"), ctr))
+
+    objects_list = []
+    cur.execute("SELECT * FROM instances")
+    for row in cur.fetchall():
+        d = collections.OrderedDict()
+        d['instance_id'] = row[1]
+        d['ctr'] = row[2]
+        objects_list.append(d)
+    j = json.dumps(objects_list)
+    print("<br />")
+    print(j)
+    writedb.commit()
+finally:
+    writedb.close()
+    readdb.close()
