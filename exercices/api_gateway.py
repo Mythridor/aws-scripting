67d2bf3806b3c09fd9df7acc23e046b3db4e2f80
Mythridor <enoubia@gmail.com>
2017-10-25 11:36:47 +0200

    Cleaning and formatting exercice

--- exercices/api_gateway.py
+++ exercices/api_gateway.py
@@ -4,18 +4,14 @@ import json
 
 import requests
 
-data = {
-    "device_name": "deviceTest"
-}
+data = {"device_name": "deviceTest"}
 
 response = requests.put("https://rkh83nul54.execute-api.eu-west-1.amazonaws.com/beta/devices", data=json.dumps(data))
 
 test = json.loads(response.content.decode('utf-8'))
 print("Device " + test['devicename'] + " (" + test['deviceid'] + ") created.")
 
-userData = {
-    "user_name": "userTest"
-}
+userData = {"user_name": "userTest"}
 
 responseUser = requests.put("https://rkh83nul54.execute-api.eu-west-1.amazonaws.com/beta/users",
                             data=json.dumps(userData))
@@ -23,22 +19,13 @@ responseUser = requests.put("https://rkh83nul54.execute-api.eu-west-1.amazonaws.
 testUser = json.loads(responseUser.content.decode('utf-8'))
 print("User " + testUser['username'] + " (" + testUser['userid'] + ") created.")
 
-simcardData = {
-    "simcard_name": "simcardTest"
-}
+simcardData = {"simcard_name": "simcardTest"}
 
 responseSimcard = requests.put("https://rkh83nul54.execute-api.eu-west-1.amazonaws.com/beta/simcards",
                                data=json.dumps(simcardData))
 
 testSimcard = json.loads(responseSimcard.content.decode('utf-8'))
 print("User " + testSimcard['simcardname'] + " (" + testSimcard['simcardid'] + ") created.")
-
-testAssociation = {
-    "type": "simcard",
-    "id": testSimcard['simcardid'],
-    "type_parent": "device_name",
-    "id_parent": test['deviceid']
-}
 
 responseSimcard = requests.post(
     "https://rkh83nul54.execute-api.eu-west-1.amazonaws.com/beta/devices/" + test['deviceid'] + "/simcards/" +
